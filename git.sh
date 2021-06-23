8dabb072375651e8efbd570821cbe9531f0c9f4c
borracciaBlu <marco.asdman@gmail.com>
2021-06-23 20:47:03 +1000

    #ZSHS-5 : update(git) : Add last files changed

--- git.sh
+++ git.sh
@@ -1,3 +1,10 @@
 alias git-dev-pull="git co develop && git pull"
 alias git-mast-pull="git co master && git pull"
 alias git-main-pull="git co main && git pull"
+
+function getLastFilesChanged {
+    LAST_HASH=$(git log --oneline | awk '{print $1}' | head -n 1)
+    git diff-tree --no-commit-id --name-only -r $LAST_HASH;
+}
+
+alias git-last-files-changed="getLastFilesChanged"
