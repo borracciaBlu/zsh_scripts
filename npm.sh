f38ccf9a27dff92a1fa592c20f0a3a42bf5af26a
Marco <marco.asdman@gmail.com>
2022-03-21 10:31:06 +1100

    #ZSH-9 : update(npm) : Check for master or main branch (#6)

--- npm.sh
+++ npm.sh
@@ -3,8 +3,8 @@ function updatePackage {
 	proj_code=$(git log --pretty="%s" | grep -e '#' | awk '{print $1}'| head -n 1 | cut -d - -f 1 | sed 's/^.//');
 	last_commit_num=$(git log --pretty="%s" | grep -e '#' | awk '{print $1}'| head -n 1 | cut -d - -f 2,4);
 	next=$(($last_commit_num + 1))
-
-	git co master
+    main_branch=$(git branch |  grep -E 'main|master' | awk '{print $2}')
+	git co $main_branch 
 	git pull
 	git co -b update/$proj_code-$next-update
 	npm update
